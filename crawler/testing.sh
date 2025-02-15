0272a0d49ab1904cfe3e61e1959d0e447747ae6e
Kiran P. Jones <f006v43@plank.thayer.dartmouth.edu>
2025-02-15 16:06:06 -0500

    git crawler fix

--- crawler/testing.sh
+++ crawler/testing.sh
@@ -32,33 +32,25 @@ mkdir ../data/wiki
 
 # 2. valgrind with moderate-sized test case
 
-myvalgrind http://cs50tse.cs.dartmouth.edu/tse/toscrape/ ../data/scrape 1
+valgrind --leak-check=full --show-leak-kinds=all ./crawler http://cs50tse.cs.dartmouth.edu/tse/toscrape/ ../data/scrape 1
 
 # 3. run over all CS50 websites with specified depths
 
 # letters with [0, 1, 2, 10]
 ./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/ ../data/letters 0
-./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/ ../data/letters 10
-
-# tests not used in testing.out
 # ./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/ ../data/letters 1
 # ./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/ ../data/letters 2
+./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/ ../data/letters 10
 
 # toscrape with [0, 1, 2, 3]
 ./crawler http://cs50tse.cs.dartmouth.edu/tse/toscrape/ ../data/scrape 0
 ./crawler http://cs50tse.cs.dartmouth.edu/tse/toscrape/ ../data/scrape 1
-
-# tests not used in testing.out
 # ./crawler http://cs50tse.cs.dartmouth.edu/tse/toscrape/ ../data/scrape 2
 # ./crawler http://cs50tse.cs.dartmouth.edu/tse/toscrape/ ../data/scrape 3
 
 # wikipedia with [0, 1, 2]
 ./crawler http://cs50tse.cs.dartmouth.edu/tse/wikipedia/ ../data/wiki 0
 ./crawler http://cs50tse.cs.dartmouth.edu/tse/wikipedia/ ../data/wiki 1
-
-# tests not used in testing.out
 # ./crawler http://cs50tse.cs.dartmouth.edu/tse/wikipedia/ ../data/wiki 2
 
-# 4. valgrind test
 
-valgrind --leak-check=full --show-leak-kinds=all ./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/ ../data/letters 10
