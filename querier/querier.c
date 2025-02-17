f63d1b66ee0193937b94b70b3c7b33be4bf875d7
Kiran P. Jones <f006v43@plank.thayer.dartmouth.edu>
2025-02-17 14:43:10 -0500

    querier intitial commit

---
+++ querier/querier.c
@@ -0,0 +1,34 @@
+/* 
+* querier.c
+* 
+* Kiran Jones, CS50 25W
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
+#include "../common/index.h"
+#include "../common/pagedir.h"
+
+
+static void parseArgs(int argc, char* argv[], char** pageDirectory, char** indexFilename);
+
+
+// ./querier pageDirectory indexFilename
+int
+main(int argc, char* argv[]) 
+{
+    return 0;
+}
+
+/*
+* Validate command-line arguments
+* Ensures that pageDirectory is crawler-produced and that indexFilename is indexer-produced 
+*/
+static void parseArgs(int argc, char* argv[], char** pageDirectory, char** indexFilename)
+{
+
+}
+
+
